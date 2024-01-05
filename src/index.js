3fdce30e8b9b3670854b4630e83deaabee1fefb6
corwin <corwinschmidt@gmail.com>
2024-01-04 18:43:59 -0600

    Core functionality

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LocationApp from './LocationApp';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LocationApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
